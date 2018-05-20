46443eac334ff6a7b7bf70039374e5db11edc641
Manuel Nielsen <manuelnielsen.5g@gmail.com>
2018-05-20 17:55:51 -0300

    TP1 - beta v1.1
    Actualizaciones:
    -Compila correctamente despues de corregir errores semánticos y sintácticos
    -Se agrega archivo resultante de la ejecución
    -Se agrega el ejecutable
    
    -No se corrobora la compilacion sobre cualquier archivo de texto

--- TP1 - Constantes Enteras/TP N°1 - main.c
+++ TP1 - Constantes Enteras/TP N°1 - main.c
@@ -6,7 +6,7 @@ typedef enum { false, true } bool; //para usar booleanos
 //Son los valores que tienen en la tabla ascii (en decimales)
 # define espacio 32
 # define enter '\n'
-# define escape 27 
+# define escape 27
 //Códigos para cada tipo de variable
 # define cero -1
 # define digOct -2
@@ -20,7 +20,7 @@ int definirTipo(char caracter){ //definir dentro de que clase de digito entra (c
 			case '0':
 				return cero;
 				break;
-			
+
 			case '1':
 			case '2':
 			case '3':
@@ -29,12 +29,12 @@ int definirTipo(char caracter){ //definir dentro de que clase de digito entra (c
 			case '7':
 				return digOct;
 				break;
-				
+
 			case '8':
 			case '9':
 				return unoDe89;
 				break;
-				
+
 			case 'a':
 			case 'b':
 			case 'c':
@@ -49,16 +49,16 @@ int definirTipo(char caracter){ //definir dentro de que clase de digito entra (c
 			case 'F':
 				return letHex;
 				break;
-			
+
 			case 'x':
 			case 'X':
 				return equis;
 				break;
-			
+
 			case espacio:
 				return espacio;
 				break;
-			
+
 			default:
 				return otro;
 				break;
@@ -68,7 +68,7 @@ int definirTipo(char caracter){ //definir dentro de que clase de digito entra (c
 
 int transicionEstado(int tipo, int estado){ // completado con la tabla de transiciones. devuelve el nuevo estado
 	int nuevoEstado = -6; //estado de rechazo
-	
+
 	switch(tipo){
 		case cero :
 			if(estado == 1 || estado == 3 || estado == 5)
@@ -87,7 +87,7 @@ int transicionEstado(int tipo, int estado){ // completado con la tabla de transi
 			else
 				nuevoEstado = estado + 1;
 			break;
-		
+
 		case unoDe89 :
 			if(estado == 0 || estado == 1)
 				nuevoEstado = 1;
@@ -96,41 +96,41 @@ int transicionEstado(int tipo, int estado){ // completado con la tabla de transi
 			else
 				nuevoEstado = 5;
 			break;
-		
+
 		case letHex :
 			if(estado == 4 || estado == 5)
 				nuevoEstado = 5;
 			else
 				nuevoEstado = -6;
 			break;
-		
+
 		case equis :
 			if(estado == 2)
 				nuevoEstado = estado + 2;
 			break;
-		
+
 		default:
 			break;
-			
+
 	}
-	
+
 	return nuevoEstado;
 }
 
 void main(){
-	
+
 	FILE *fInput;
 	FILE *fOutput;
-	
+
 	char caracter;
 	int tipo, estado=0, i=0;
 	bool finPalabra = false, finRenglon = false, terminarPalabra = false;
-	
+
 	char noConstante[24] = " No es constante entera";
 	char cteDecimal[9] = " Decimal";
 	char cteOctal[7] = " Octal";
 	char cteHexa[13] = " Hexadecimal";
-	
+
 	fInput = fopen ("input.txt", "r");
 	fOutput = fopen("output.txt", "w");
 
@@ -142,83 +142,87 @@ void main(){
 		switch(tipo){
 			case cero:
 			case digOct:
-				estado = transicionarEstado(tipo, estado);
+				estado = transicionEstado(tipo, estado);
 				break;
 			case unoDe89:
 				if(estado != 2 || estado !=3 )
-					estado = transicionarEstado(tipo, estado);
+					estado = transicionEstado(tipo, estado);
 				else
 					terminarPalabra = true;
 				break;
 			case letHex:
 				if(estado == 4 || estado == 5 )
-					estado = transicionarEstado(tipo, estado);
+					estado = transicionEstado(tipo, estado);
 				else
 					terminarPalabra = true;
 				break;
 			case equis:
 				if(estado == 2 )
-					estado = transicionarEstado(tipo, estado);
+					estado = transicionEstado(tipo, estado);
 				else
 					terminarPalabra = true;
 				break;
-			case espacio: 
+			case espacio:
 				finPalabra = true;
 				break;
 			default:
 
-				while(!feof(fInput) && tipo != espacio){ //termina de escribir la palabra 
-					fwrite(caracter,sizeof(char),1,fOutput);
+				while(!feof(fInput) && tipo != espacio){ //termina de escribir la palabra
+					fwrite(&caracter,sizeof(char),1,fOutput);
 					caracter = getc(fInput);
 					tipo = definirTipo(caracter);
 				}
-				
+
 				do{ //escribe " No es constante entera", seguido de la palabra
-					fwrite(noConstante[i],sizeof(char),1,fOutput);
+					fwrite(&noConstante[i],sizeof(char),1,fOutput);
 					i++;
 				}while(noConstante[i] != '\0');
 				i=0;
 
-				fwrite('\n',sizeof(char),1,fOutput); //escribir salto de línea
-				
+                caracter = '\n';
+				fwrite(&caracter,sizeof(char),1,fOutput); //escribir salto de línea
+
 				finRenglon = true;
-				
+
 				break;
 		}
-		
-		if(!finRenglon || feof(fInput)){			
+
+		if(!finRenglon || feof(fInput)){
 			if(!finPalabra)
-				fwrite(caracter,sizeof(char),1,fOutput);
+				fwrite(&caracter,sizeof(char),1,fOutput);
 			else{
 				if(estado == 2 || estado == 3){
 					do{ //escribe " Octal" seguido de la palabra
-						fwrite(cteOctal[i],sizeof(char),1,fOutput);
+						fwrite(&cteOctal[i],sizeof(char),1,fOutput);
 						i++;
 					}while(cteOctal[i] != '\0');
 				}else if(estado == 1){
 					do{ //escribe " Decimal" seguido de la palabra
-						fwrite(cteDecimal[i],sizeof(char),1,fOutput);
+						fwrite(&cteDecimal[i],sizeof(char),1,fOutput);
 						i++;
 					}while(cteDecimal[i] != '\0');
 				}else{
 					do{ //escribe " Hexadecimal" seguido de la palabra
-						fwrite(cteHexa[i],sizeof(char),1,fOutput);
+						fwrite(&cteHexa[i],sizeof(char),1,fOutput);
 						i++;
 					}while(cteHexa[i] != '\0');
 				}
-				i=0;
-				fwrite('\n',sizeof(char),1,fOutput); //escribir salto de línea
 				
+				estado = 0;
+				i=0;
+				caracter = '\n';
+				fwrite(&caracter,sizeof(char),1,fOutput); //escribir salto de línea
+
 				finPalabra = false;
 			}
 		}
 		else //se reinicia para leer una nueva palabra
 			finRenglon = false;
-		
+
 	}
 
-	fclose (ptrArchLectura);
-	fclose (ptrArchEscritura);
+	fclose (fInput);
+	fclose (fOutput);
 
 	return;
 }
