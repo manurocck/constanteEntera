e9598a103e8874e3c5e15e51f416f9a69eb68d6d
Leonardo <39105908+LAF96@users.noreply.github.com>
2018-11-26 13:37:35 -0300

    Update lex.yy.c

--- TP3 - calculadoraBilingue/lex.yy.c
+++ TP3 - calculadoraBilingue/lex.yy.c
@@ -282,12 +282,12 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 10
-#define YY_END_OF_BUFFER 11
-static yyconst short int yy_accept[18] =
+#define YY_NUM_RULES 12
+#define YY_END_OF_BUFFER 13
+static yyconst short int yy_accept[20] =
     {   0,
-        0,    0,   11,   10,    9,    6,    4,    5,    7,   10,
-        1,    8,    2,    0,    1,    3,    0
+        0,    0,   13,   12,   11,    9,   10,    6,    4,    5,
+        7,   12,    1,    8,    2,    0,    1,    3,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -295,16 +295,16 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
+        4,    5,    6,    1,    7,    1,    8,    9,   10,   10,
+       10,   10,   10,   10,   10,   11,   11,    1,    1,    1,
+        1,    1,    1,    1,   12,   12,   12,   12,   12,   12,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    4,    1,    5,    1,    6,    7,    8,    8,
-        8,    8,    8,    8,    8,    9,    9,    1,    1,    1,
-        1,    1,    1,    1,   10,   10,   10,   10,   10,   10,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,   11,    1,    1,
-        1,    1,    1,   12,    1,    1,   10,   10,   10,   10,
+        1,    1,    1,    1,    1,    1,    1,   13,    1,    1,
+        1,    1,    1,   14,    1,    1,   12,   12,   12,   12,
 
-       10,   10,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,   11,
+       12,   12,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,   13,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,38 +322,40 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[13] =
+static yyconst int yy_meta[15] =
     {   0,
-        1,    1,    1,    1,    1,    1,    2,    2,    3,    4,
-        5,    1
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    2,
+        3,    4,    5,    1
     } ;
 
-static yyconst short int yy_base[21] =
+static yyconst short int yy_base[23] =
     {   0,
-        0,    0,   22,   23,   23,   23,   23,   23,   23,    4,
-        0,   23,    3,    0,    0,    0,   23,   11,   15,   17
+        0,    0,   19,   26,   26,   26,   26,   26,   26,   26,
+       26,    5,    0,   26,    6,    0,    0,    0,   26,   15,
+       19,   21
     } ;
 
-static yyconst short int yy_def[21] =
+static yyconst short int yy_def[23] =
     {   0,
-       17,    1,   17,   17,   17,   17,   17,   17,   17,   18,
-       19,   17,   18,   20,   19,   20,    0,   17,   17,   17
+       19,    1,   19,   19,   19,   19,   19,   19,   19,   19,
+       19,   20,   21,   19,   19,   22,   21,   22,    0,   19,
+       19,   19
     } ;
 
-static yyconst short int yy_nxt[36] =
+static yyconst short int yy_nxt[41] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,   11,    4,
-        4,   12,   13,   17,   14,   13,   15,   15,   16,   16,
-       16,   17,    3,   17,   17,   17,   17,   17,   17,   17,
-       17,   17,   17,   17,   17
+        4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
+       13,    4,    4,   14,   15,   15,   15,   16,   19,   15,
+       17,   17,   18,   18,   18,    3,   19,   19,   19,   19,
+       19,   19,   19,   19,   19,   19,   19,   19,   19,   19
     } ;
 
-static yyconst short int yy_chk[36] =
+static yyconst short int yy_chk[41] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,   18,   13,   10,   18,   19,   19,   20,   20,
-       20,    3,   17,   17,   17,   17,   17,   17,   17,   17,
-       17,   17,   17,   17,   17
+        1,    1,    1,    1,   15,   15,   20,   12,    3,   20,
+       21,   21,   22,   22,   22,   19,   19,   19,   19,   19,
+       19,   19,   19,   19,   19,   19,   19,   19,   19,   19
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -367,14 +369,14 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "calculadoraFLEX.l"
+#line 1 "calculadoraBISON.l"
 #define INITIAL 0
-#line 2 "calculadoraFLEX.l"
+#line 2 "calculadoraBISON.l"
 #include "calculadoraBISON.tab.h"
 #include <math.h>
 #include <stdio.h>
 #include <ctype.h>
-#line 378 "lex.yy.c"
+#line 380 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -525,10 +527,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 8 "calculadoraFLEX.l"
+#line 8 "calculadoraBISON.l"
 
 
-#line 532 "lex.yy.c"
+#line 534 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -579,13 +581,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 18 )
+				if ( yy_current_state >= 20 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 23 );
+		while ( yy_base[yy_current_state] != 26 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -613,55 +615,65 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 10 "calculadoraFLEX.l"
-{ return DECNUM; }
+#line 10 "calculadoraBISON.l"
+{yylval = atoi(yytext); return DECNUM; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 11 "calculadoraFLEX.l"
-{ return OCTNUM; }
+#line 11 "calculadoraBISON.l"
+{yylval = octalAdecimal(yytext); return OCTNUM; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 12 "calculadoraFLEX.l"
-{ return HEXNUM; }
+#line 12 "calculadoraBISON.l"
+{ yylval = hexaAdecimal(yytext); return HEXNUM; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 13 "calculadoraFLEX.l"
+#line 13 "calculadoraBISON.l"
 {return '+';}
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 14 "calculadoraFLEX.l"
+#line 14 "calculadoraBISON.l"
 {return '-';}
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 15 "calculadoraFLEX.l"
+#line 15 "calculadoraBISON.l"
 {return '*';}
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 16 "calculadoraFLEX.l"
+#line 16 "calculadoraBISON.l"
 {return '/';}
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 17 "calculadoraFLEX.l"
+#line 17 "calculadoraBISON.l"
 {return '^';}
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 18 "calculadoraFLEX.l"
-{return '\n';}
+#line 18 "calculadoraBISON.l"
+{return '(';}
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 20 "calculadoraFLEX.l"
+#line 19 "calculadoraBISON.l"
+{return ')';}
+	YY_BREAK
+case 11:
+YY_RULE_SETUP
+#line 20 "calculadoraBISON.l"
+{return '\n';}
+	YY_BREAK
+case 12:
+YY_RULE_SETUP
+#line 22 "calculadoraBISON.l"
 ECHO;
 	YY_BREAK
-#line 665 "lex.yy.c"
+#line 677 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -953,7 +965,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 18 )
+			if ( yy_current_state >= 20 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -988,11 +1000,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 18 )
+		if ( yy_current_state >= 20 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 17);
+	yy_is_jam = (yy_current_state == 19);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1547,6 +1559,116 @@ int main()
 	return 0;
 	}
 #endif
-#line 20 "calculadoraFLEX.l"
+#line 22 "calculadoraBISON.l"
 
 
+int caracterHexaADecimal(char c){
+    switch(c){
+        case '0':
+            return 0 ;
+        case '1':
+            return 1 ;
+        case '2':
+            return 2 ;
+        case '3':
+            return 3 ;
+        case '4':
+            return 4 ;
+        case '5':
+            return 5 ;
+        case '6':
+            return 6 ;
+        case '7':
+            return 7 ;
+        case '8':
+            return 8 ;
+        case '9':
+            return 9 ;
+        case 'A':
+        case 'a':
+            return 10 ;
+        case 'B':
+        case 'b':
+            return 11 ;
+        case 'C':
+        case 'c':
+            return 12 ;
+        case 'D':
+        case 'd':
+            return 13 ;
+        case 'E':
+        case 'e':
+            return 14 ;
+        case 'F':
+        case 'f':
+            return 15 ;
+        case '\n':
+        case '\0':
+            return 0 ;
+        break;
+    }
+    return 0 ;
+}
+
+int caracterOctalADecimal(char c){
+    switch(c){
+        case '0':
+            return 0 ;
+        case '1':
+            return 1 ;
+        case '2':
+            return 2 ;
+        case '3':
+            return 3 ;
+        case '4':
+            return 4 ;
+        case '5':
+            return 5 ;
+        case '6':
+            return 6 ;
+        case '7':
+            return 7 ;
+        case '8':
+            return 8 ;
+        case '\n':
+        case '\0':
+            return 0 ;
+        break;
+    }
+    return 0 ;
+}
+
+int longitudCadena(char cadena[]){
+    int contador = 0 ;
+    while(cadena[contador]!='\0'){
+        contador++ ;
+    }
+    return contador ;
+}
+
+int hexaAdecimal(char cadena[]){ // convierte un string del tipo [a-a]{2}[a-fA-F0-9]* a decimal
+    int punteroFinDeCadena = longitudCadena(cadena) - 3 ;
+    int posicionReal = punteroFinDeCadena ;
+    int posicionPuntero = 2 ;
+    int numHexadecimal = 0 ;
+    while (posicionReal>=0) { // se saltea el 0x
+        numHexadecimal += caracterHexaADecimal(cadena[posicionPuntero]) * pow(16,posicionReal);
+        posicionPuntero++ ;
+        posicionReal-- ;
+    }
+
+    return numHexadecimal ;
+}
+
+int octalAdecimal(char cadena[]){
+    int punteroFinDeCadena = longitudCadena(cadena) - 2 ; //1 porque está la posicion 0 + 1 p/caracter 0 con el que empieza todo octal
+    int posicionReal = punteroFinDeCadena ;
+    int posicionPuntero = 1 ; //porque todo octal empieza en 0
+    int numOctal = 0 ;
+    while (posicionReal>=0) {
+        numOctal += caracterOctalADecimal(cadena[posicionPuntero]) * pow(8,posicionReal);
+        posicionPuntero++ ;
+        posicionReal-- ;
+    }
+    return numOctal ;
+}
